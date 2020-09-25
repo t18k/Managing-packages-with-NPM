8c7ebe5c414bd30140ac68258f695808cda5b910
t18k <thirukumaran953@gmail.com>
2020-09-25 18:52:58 +0530

    Setting up app for basic node and express

--- server.js
+++ server.js
@@ -7,6 +7,8 @@
 
 var fs = require('fs');
 var express = require('express');
+var bGround = require('fcc-express-bground');
+var myApp = require('./myApp');
 var app = express();
 
 if (!process.env.DISABLE_XORIGIN) {
@@ -53,7 +55,8 @@ app.use(function(err, req, res, next) {
   }  
 })
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+var port = process.env.PORT || 3000;
+bGround.setupBackgroundApp(app, myApp, __dirname).listen(port || 3000, function () {
+  bGround.log('Node is listening on port '+ port + '...');
 });
 
