7825fd682cab0e02744924bb457f9bbf7408a6e1
t18k <thirukumaran953@gmail.com>
2020-09-26 12:12:28 +0530

    Add a route to send Hello Express

--- server.js
+++ server.js
@@ -55,6 +55,10 @@ if (!process.env.DISABLE_XORIGIN) {
 //   }  
 // })
 
+app.get('/', function(req, res) {
+  res.send('Hello Express');
+});
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port || 3000, function () {
   bGround.log('Node is listening on port '+ port + '...');
