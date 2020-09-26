4c62db1d3862fd8a40914613f49347d0c3b476c1
t18k <thirukumaran953@gmail.com>
2020-09-26 12:42:41 +0530

    Added middleware[css file] through app.use()

--- server.js
+++ server.js
@@ -59,6 +59,8 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.use('/public', express.static(__dirname + '/public'));
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port || 3000, function () {
   bGround.log('Node is listening on port '+ port + '...');
